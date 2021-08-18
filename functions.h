8a8c6c7130976a38fe8739e49326b9ad1620a15c
shiramar <86928995+shiramar@users.noreply.github.com>
2021-08-19 00:15:17 +0300

    Update functions.h

--- functions.h
+++ functions.h
@@ -5,30 +5,53 @@
 #include "assembler.h"
 #include "struct.h"
 
-/* In this file we declare all the fuctions in common.c, struct.c and externalStructs.c */
+/*Declarations of functions found in type files '.c'*/
 
-/* Functions from common.c */
+/**functions.c functions**/ 
+
+/*This functions creat the file name*/
 char* create_file_name(char* original, int type);
+
 int ignore_line(char* line);
+
 int if_error();
+
 char* skip_spaces(char* ch);
+
 int end_of_line(char* line);
+
 void copy_word(char* destination, char* line);
+
 unsigned int extract_bits(unsigned int word, int start, int end);
+
 unsigned int int_to_hex(unsigned int word,int i);
+
 boolean is_label(char* sign, int colon);
+
 char* next_word(char* seq);
+
 boolean is_register(char* sign);
+
 int find_reg_number(char* sign);
+
 int find_index(char* sign, const char* arr[], int n);
+
 int find_guidance(char* sign);
+
 int find_command(char* sign);
+
 char* next_list_word(char* dest, char* line);
+
 boolean is_number(char* seq);
+
 char* next_word_string(char* dest, char* line);
+
 boolean is_string(char* string);
+
 void add_string_to_data_image(char* str);
+
 void add_to_instructions_image(unsigned int word);
+
 void print_error(int line_number);
 
 /* Functions from struct.c */
